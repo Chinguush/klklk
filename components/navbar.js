97b60be3768dd4093468e856ac020183ad1bb43a
Chinguush <uchinguush59@gmail.com>
2022-04-18 10:13:42 +0800

    hha

--- components/navbar.js
+++ components/navbar.js
@@ -1,33 +1,57 @@
 import React from "react";
+import { useState } from 'react'
+
 
 const NavBar = () => {
+    var [active, setActive] = useState('home')
+    var classActive = 'active'
+    // const list = document.querySelectorAll('.list');
+    // function activeLink() {
+    //     list.forEach((item) => 
+    //     item.classList.remove('active'));
+    //     this.classList.add('active')
+    // }
+    // list.forEach((item) => 
+    // item.addEventListener('click', activeLink));
+      
     return (
-        <div className="home1">
-            <div className="home11">
-                <div className="ballcont1">
-                    <i className="fas fa-home"></i>
-                    <div className="ball"></div>
-                </div>
-            </div>
-            <div className="home11">
-                <div>
-                    <i className="fa fa-chart-bar"></i>
-                    <div className="ball"></div>    
-                </div>
-            </div>
-            <div className="home11">
-                <div>
-                    <i className="fa fa-users"></i>
-                    <div className="ball"></div>
-                </div>
-            </div>
-            <div className="home11">
-                <div>
-                    <i className="fa fa-cog"></i>
-                    <div className="ball"></div>    
-                </div>
-            </div>
-        </div>
+        <div className="navigation">
+            <ul>
+                <li className={"list" + active === "home" ? " active": ""}>
+                    <a href="#" onClick={() => setActive('home')}>
+                        <span className="icon">
+                            <i class="fa-solid fa-house"></i>
+                        </span>
+                        <span className="ball"></span>
+                    </a>
+                </li>
+                <li className={"list" + active === "chart" ? "active": ""}>
+                    <a href="#" onClick={() => setActive('chart')}>
+                        <span className="icon">
+                            <i class="fa-solid fa-chart-line"></i>
+                        </span>
+                        <span className="ball"></span>
+                    </a>
+                </li>
+                <li className={"list" + active === "group" ? "active": ""}>
+                    <a href="#" onClick={() => setActive('group')}>
+                        <span className="icon">
+                            <i class="fa-solid fa-users"></i>
+                        </span>
+                        <span className="ball"></span>
+                    </a>
+                </li>
+                <li className={"list" + active === "settings" ? "active": ""}>
+                    <a href="#" onClick={() => setActive('settings')}>
+                        <span className="icon">
+                            <i class="fa-solid fa-gear"></i>
+                        </span>
+                        <span className="ball"></span>
+                    </a>
+                </li>
+                <div className="indicator"></div>
+            </ul>
+        </div> 
     )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"></link>
                     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
-                    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.cs"></link>
+                    <script src="https://kit.fontawesome.com/498b5f2186.js"></script>
                 </Head>
                 <body>
                     <Main />
