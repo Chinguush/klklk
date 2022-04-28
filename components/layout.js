8260010e417eb5ced6a5012f0628ae2dc2d6829c
Chinguush <uchinguush59@gmail.com>
2022-04-28 08:46:45 +0800

    hehe

--- components/layout.js
+++ components/layout.js
@@ -13,16 +13,14 @@ const Layout = ({children}) => {
     }
     return (
         <div className="container">
-            {children}
-            <Navbar></Navbar>
             <div>
                 <div className="header">
                     <div className="header1">                        
                     </div>
-                    <div className={"logocont" + isActive('/settings')}>
-                        <div className="logo">
-                        </div>
+                <div className={"logocont" + isActive('/settings')}>
+                    <div className="logo">
                     </div>
+                </div>
                 </div>
                 <div className={"negcont" + isActive('/settings')}>
                     <div className="neg">
@@ -31,13 +29,93 @@ const Layout = ({children}) => {
                                 ₮ 3'500'000
                             </div>
                             <div className="neg12">
+                                Нийт мөнгөн дүн
                             </div>
                         </div>
                         <div className="neg2">
+                            <div className="neg21">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={"hoyrcont"  + isActive('/settings')}>
+                    <div className="hoyr">
+                        <div className="hoyr1">
+                            <i className="fa-solid fa-wallet"></i>
+                        </div>
+                        <div className="hoyr2">
+                            <div class="hoyr2zasvar">
+                                <div className="hoyr21">
+                                    Нэмэлт мэдээлэл
+                                </div>
+                                <div className="hoyr22">
+                                    ₮ 20 000
+                                </div>
+                                <div className="hoyr23">
+                                    Хэтэвч
+                                </div>
+                            </div>
+                        </div>
+                        <div className="hoyr3">
+                            <button className="hoyr31">
+                                +
+                            </button>  
+                        </div>
+                    </div>
+                </div>
+                <div className={"guravcont"  + isActive('/settings')}>
+                    <div className="gurav">
+                        <div className="gurav1">
+                            <i class="fa-solid fa-credit-card"></i>
+                        </div>
+                        <div className="gurav2">
+                            <div class="gurav2zasvar">
+                                <div className="gurav21">
+                                    Нэмэлт мэдээлэл
+                                </div>
+                            <div className="gurav22">
+                                ₮ 20 000
+                            </div>
+                            <div className="gurav23">
+                                Карт
+                            </div>
+                            </div>
+                        </div>
+                        <div className="gurav3">
+                            <button className="gurav31">
+                                +
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className={"dorovcont" + isActive('/settings')}>
+                    <div className="dorov">
+                        <div className="dorov1">
+                            <i class="fa-solid fa-coins"></i>
+                        </div>
+                        <div className="dorov2">
+                            <div class="dorov2zasvar">
+                                <div className="dorov21">
+                                    Нэмэлт мэдээлэл
+                                </div>
+                                <div className="dorov22">
+                                    ₮ 20 000
+                                </div>
+                                <div className="dorov23">
+                                    Хадгаламж
+                                </div>
+                            </div>
+                        </div>
+                        <div className="dorov3">
+                            <button className="dorov31">
+                                +
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
+            {children}
+            <Navbar></Navbar>
         </div>
     )
 }
--- pages/settings.js
+++ pages/settings.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import styles from '../styles/Settings.module.css'
 
-export default function settings() {
+const Settings = () => {
   return (
-    <div  className='main'>
-      <div className="bodyOfSettings"></div>
+    <div className={styles.father}>
+      <div className={styles.profile}></div>
+      <ul>
+        <li className={styles.homePage}>
+          <a href='#'>
+            <span className={styles.iconS}>
+              <i className="fa-regular fa-circle-user"></i>
+            </span>
+            <span className={styles.textS}> Нүүр хуудас {'>'} </span>
+          </a>
+        </li>
+        {/*  */}
+        <li className={styles.group}>
+          <a href='#'>
+            <span className={styles.iconS}>
+              <i class="fa-solid fa-user-group"></i>
+            </span>
+            <span className={styles.textS}> Бүлэг {'>'} </span>
+          </a>
+        </li>
+        <li className={styles.currency}>
+          <a href='#'>
+            <span  className={styles.iconS}>
+              <i class="fa-brands fa-bitcoin"></i>
+            </span>
+            <span className={styles.textS}> Тэмдэг {'>'} </span>
+          </a>
+        </li>
+        <li className={styles.addCard}>
+          <a href='#'>
+            <span  className={styles.iconS}>
+              <i class="fa-solid fa-credit-card"></i>  
+            </span>
+            <span className={styles.textS}> Карт нэмэх {'>'} </span>
+          </a>
+        </li>
+        <li className={styles.logOut}>
+          <a href='#'>
+            <span  className={styles.iconS}>
+              <i class="fa-solid fa-right-from-bracket"></i>
+            </span>
+            <span className={styles.textS}> Гарах {'>'} </span>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
+
+export default Settings;
