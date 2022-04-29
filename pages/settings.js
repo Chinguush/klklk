ffac9f48607ff4b9921d37f9815d642526bb64f6
bobogng <mamba0323@yahoo.com>
2022-04-29 11:20:15 +0800

    bobo

--- pages/settings.js
+++ pages/settings.js
@@ -3,50 +3,66 @@ import styles from '../styles/Settings.module.css'
 
 const Settings = () => {
   return (
-    <div className={styles.father}>
-      <div className={styles.profile}></div>
-      <ul>
-        <li className={styles.homePage}>
-          <a href='#'>
-            <span className={styles.iconS}>
-              <i className="fa-regular fa-circle-user"></i>
-              <span className={styles.textS}> Нүүр хуудас {'>'} </span>
-            </span>
-          </a>
-        </li>
-        <li className={styles.group}>
-          <a href='#'>
-            <span className={styles.iconS}>
-              <i class="fa-solid fa-user-group"></i>
-              <span className={styles.textS}> Бүлэг {'>'} </span>
-            </span>
-          </a>
-        </li>
-        <li className={styles.currency}>
-          <a href='#'>
-            <span  className={styles.iconS}>
-              <i class="fa-brands fa-bitcoin"></i>
-              <span className={styles.textS}> Тэмдэг {'>'} </span>
-            </span>
-          </a>
-        </li>
-        <li className={styles.addCard}>
-          <a href='#'>
-            <span  className={styles.iconS}>
-              <i class="fa-solid fa-credit-card"></i>
-              <span className={styles.textS}> Карт нэмэх {'>'} </span>
-            </span>            
-          </a>
-        </li>
-        <li className={styles.logOut}>
-          <a href='#'>
-            <span  className={styles.iconS}>
-              <i class="fa-solid fa-right-from-bracket"></i>   
-              <span className={styles.textS}> Гарах {'>'} </span>
-            </span>
-          </a>
-        </li>
-      </ul>
+    // <div className={styles.father}>
+    //   <div className={styles.profile}></div>
+    //   <ul>
+    //     <li className={styles.homePage}>
+    //       <a href='#'>
+    //         <span className={styles.iconS}>
+    //           <i className="fa-regular fa-circle-user"></i>
+    //           <span className={styles.textS}> Нүүр хуудас {'>'} </span>
+    //         </span>
+    //       </a>
+    //     </li>
+    //     <li className={styles.group}>
+    //       <a href='#'>
+    //         <span className={styles.iconS}>
+    //           <i class="fa-solid fa-user-group"></i>
+    //           <span className={styles.textS}> Бүлэг {'>'} </span>
+    //         </span>
+    //       </a>
+    //     </li>
+    //     <li className={styles.currency}>
+    //       <a href='#'>
+    //         <span  className={styles.iconS}>
+    //           <i class="fa-brands fa-bitcoin"></i>
+    //           <span className={styles.textS}> Тэмдэг {'>'} </span>
+    //         </span>
+    //       </a>
+    //     </li>
+    //     <li className={styles.addCard}>
+    //       <a href='#'>
+    //         <span  className={styles.iconS}>
+    //           <i class="fa-solid fa-credit-card"></i>
+    //           <span className={styles.textS}> Карт нэмэх {'>'} </span>
+    //         </span>            
+    //       </a>
+    //     </li>
+    //     <li className={styles.logOut}>
+    //       <a href='#'>
+    //         <span  className={styles.iconS}>
+    //           <i class="fa-solid fa-right-from-bracket"></i>   
+    //           <span className={styles.textS}> Гарах {'>'} </span>
+    //         </span>
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </div>
+    <div className={styles.goy}>
+      <div className={styles.goyzuragbagtaamj}>
+          <div className={styles.goyzuraas}>
+
+          </div>
+          <div className={styles.goyzurag}>
+
+          </div>
+          <div className={styles.goyzuraas1}>
+
+          </div>
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
